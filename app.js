e11c90e50b0bcbbe6b0eb33868ccdec3111de2a0
espehel <espenhellerud5@gmail.com>
2015-05-16 23:48:47 +0200

    files for adding new gather

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var create = require('./routes/create');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/gather/create',create);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
