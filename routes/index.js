555ae54e56253833633362f1c55dbcd030ea9ac1
espehel <espenhellerud5@gmail.com>
2015-05-19 17:23:08 +0200

    skeleton for user login

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CS:GO Gather system' });
 });
 
+// =====================================
+// LOGIN ===============================
+// =====================================
+// show the login form
+router.get('/login', function(req, res) {
+
+  // render the page and pass in any flash data if it exists
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+// process the login form
+// router.post('/login', do all our passport stuff here);
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+router.get('/signup', function(req, res) {
+
+  // render the page and pass in any flash data if it exists
+  res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// process the signup form
+// router.post('/signup', do all our passport stuff here);
+
 module.exports = router;
